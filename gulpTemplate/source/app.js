861946c18c6d5850a621843cc1005fb1e0c7b57a
scurto <scurto@mail.ua>
2017-03-01 12:19:10 +0200

    added 'gulp template'

---
+++ gulpTemplate/source/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by yustymenko on 01.03.2017.
+ */
+angular.module('app', [])
+
+.config(function() {
+
+});
---
+++ gulpTemplate/source/components/msgShowDirective.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yustymenko on 01.03.2017.
+ */
+// (function() {
+//     angular.module('app')
+//         .directive('msgShow', function() {
+//             return {
+//                 restrict: 'E',
+//                 template: [].join(''),
+//             }
+//         })
+// })()
