512fe4ed9334d799f3249e283ed7e5f7653a910b
scurto <scurto@mail.ua>
2017-03-09 00:34:36 +0200

    added 'toggle' and 'delete'

--- introProject/app/app.component.ts
+++ introProject/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from "@angular/core";
 
+const todos = [
+    {
+        title: "Learn javascript",
+        completed: true
+    },
+    {
+        title: "Learn angular2",
+        completed: true
+    },
+    {
+        title: "Write application",
+        completed: false
+    }
+];
 
 @Component({
     moduleId: module.id,
@@ -9,4 +23,17 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
     title = "Hello Angular";
+    componentTasks = todos;
+
+    toggle(todo: any) {
+        todo.completed = !todo.completed;
+    }
+
+    delete(todo: any) {
+        let index = this.componentTasks.indexOf(todo);
+
+        if (index > -1) {
+            this.componentTasks.splice(index, 1);
+        }
+    }
 }
