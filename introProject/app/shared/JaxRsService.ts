7ab0378314fb4841267d370b358f2ccc01c9780a
scurto <scurto@mail.ua>
2017-06-20 16:42:53 +0300

    added 'delete' & 'update 'angular+hibernate

--- introProject/app/shared/JaxRsService.ts
+++ introProject/app/shared/JaxRsService.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "@angular/core";
 import {Http, Headers} from "@angular/http";
+import {JaxRsTask} from "./JaxRsTask";
 /**
  * Created by yustymenko on 15.06.2017.
  */
@@ -33,6 +34,35 @@ export class JaxRsService {
 
         return this._http.post(
             this.mySrever + 'webapi/get_test_json_hiber',
+            {
+                headers: headers
+            }
+        ).map(res => res);
+    }
+
+    removeTask(task: JaxRsTask) {
+        var headers = new Headers();
+        // headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        headers.append('Content-Type', 'application/json');
+        var json = JSON.stringify(task);
+        return this._http.post(
+            this.mySrever + 'webapi/remove_task_hiber',
+            json,
+            {
+                headers: headers
+            }
+        ).map(res => res);
+    }
+
+    updateTask(task: JaxRsTask) {
+        var headers = new Headers();
+        // headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        headers.append('Content-Type', 'application/json');
+        var json = JSON.stringify(task);
+        console.log("JSON", json);
+        return this._http.post(
+            this.mySrever + 'webapi/update_task_hiber',
+            json,
             {
                 headers: headers
             }
--- introProject/app/shared/JaxRsTask.ts
+++ introProject/app/shared/JaxRsTask.ts
@@ -6,5 +6,5 @@ export class JaxRsTask {
     created: number;
     id: number;
     message: string;
-    completed: boolean;
+    completed: number;
 }
